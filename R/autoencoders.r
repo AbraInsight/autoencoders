25d7f75056aa14465dfdba47c5cdc1ce02788421
hamaadshah <hamaadshah@hotmail.com>
2017-11-24 17:16:26 +0000

    Transfer learning for CIFAR10.

--- R/autoencoders.r
+++ R/autoencoders.r
@@ -2,11 +2,13 @@ install.packages(pkgs = "https://cran.r-project.org/bin/macosx/el-capitan/contri
                  lib = .libPaths()[1], 
                  repos = NULL)
 
-invisible(lapply(X = list("insuranceData", "data.table"), 
+libs.to.load <- list("insuranceData",
+                     "data.table")
+
+invisible(lapply(X = libs.to.load, 
                  FUN = function(x) library(package = x, 
                                            lib.loc = .libPaths()[1],
                                            character.only = TRUE)))
 
 data(ClaimsLong)
-
 ins.data.dt <- data.table(ClaimsLong)
